6f6c611471e37f8d52718ae23986cf6056257e58
David Gaither <gaither@protonmail.com>
2024-02-04 14:55:33 -0500

    Py and JS programs fully functional.  Testing not set up but test cases pass manual testing

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,8 +1,51 @@
-exports.isCharacterMatch = function(string1, string2) {
+// exports.isCharacterMatch = function(string1, string2) {
 
-};
+// };
 
-exports.anagramsFor = function(word, listOfWords) {
+// exports.anagramsFor = function(word, listOfWords) {
 
-};
+// };
 
+function isCharacterMatch(string1, string2){
+    
+    let match1 = ""
+    let match2 = ""
+
+    for (const char of string1.toLowerCase()) {
+        if (char.match(/[a-z0-9]/) && string2.toLowerCase().includes(char)) {
+            match1 += char;
+        }
+    }
+    
+    for (const char of string2.toLowerCase()) {
+        if (char.match(/[a-z0-9]/) && string1.toLowerCase().includes(char)) {
+            match2 += char;
+        }
+    }
+
+    if (match1.length == match2.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function anagramsFor(word, listOfWords) {
+    
+    let result = []
+    
+    for (const item of listOfWords) {
+        let matches = "";
+        for (const char of item) {
+            if (word.includes(char)) {
+                matches += char
+            }
+        }
+        if (matches.length == word.length) {
+            result.push(matches);
+        }
+    }
+    return result;
+}
+
+module.exports = { isCharacterMatch, anagramsFor };
